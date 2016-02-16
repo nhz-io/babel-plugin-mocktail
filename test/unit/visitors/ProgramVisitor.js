637f1a1c942a133dd73b6f49068edf9970f70173
Ishi Ruy <dev@nhz.io>
2016-02-17 00:44:12 +0200

    added chai and populated few tests

--- test/unit/visitors/ProgramVisitor.js
+++ test/unit/visitors/ProgramVisitor.js
@@ -1,14 +1,14 @@
-import { should, assert, expect } from "should"
+import {Should, expect, assert} from "chai"
 import ASTVisitor from "../../../src/visitors/ASTVisitor"
-import ProgramVisitor from "../../src/visitors/ProgramVisitor"
+import ProgramVisitor from "../../../src/visitors/ProgramVisitor"
 
-should()
+Should()
 
 /** @test {ProgramVisitor} */
 describe("ProgramVisitor", () => {
 
   it('should be a subclass of ASTVisitor', () => {
-    assert(ProgramVisitor.prototype instanceof ASTVisitor)
+    ProgramVisitor.prototype.should.be.an.instanceof(ASTVisitor)
   })
 
   /** @test {ProgramVisitor#constructor} */
@@ -16,8 +16,14 @@ describe("ProgramVisitor", () => {
     let visitor
     beforeEach(() => visitor = new ProgramVisitor())
 
-    it("should call a super constructor", () => {
+    it("populates ProgramVisitor~nestedVisitors from the first argument", () => {
+      const One = {}, Two = {}, nestedVisitors = { One, Two }
 
+      visitor = new ProgramVisitor(nestedVisitors)
+      visitor.nestedVisitors.should.be.ok
+      visitor.nestedVisitors.should.not.be.equal(nestedVisitors)
+      visitor.nestedVisitors.One.should.be.equal(One)
+      visitor.nestedVisitors.Two.should.be.equal(Two)
     })
 
     it("should initialize a state property", () => {
@@ -27,11 +33,27 @@ describe("ProgramVisitor", () => {
 
   })
 
+  describe("~state", () => {
+    let visitor
+    const properties = {
+      "name"    : String,
+      "path"    : String,
+      "imports" : Array,
+      "exports" : Array
+    }
+    beforeEach(() => visitor = new ProgramVisitor())
+
+    Object.keys(properties).forEach((name) => {
+      const type = properties[name]
+      it(`should have '${name}' property of type '${type.name}'`, () => {
+        visitor.state[name].should.be.ok.and.be.an.instanceof(type)
+      })
+    })
+
+  })
+
   /** @test {ProgramVisitor#enter} */
   describe("#enter(path, state)", () => {
-    it("should throw a SyntaxError", () => {
-      const visitor = new ProgramVisitor()
-      should(() => (new ProgramVisitor).enter()).throw(SyntaxError)
-    })
+
   })
 })
