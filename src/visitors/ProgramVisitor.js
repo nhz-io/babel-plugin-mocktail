6384c3fbee0079729cda9c64f0fb3875250d658b
Ishi Ruy <dev@nhz.io>
2016-02-16 23:33:09 +0200

    added ProgramVisitor stub (again)

---
+++ src/visitors/ProgramVisitor.js
@@ -0,0 +1,15 @@
+import ASTVisitor from './ASTVisitor'
+/**
+  * @example
+  * const Program = new ProgramVisitor({ ... })
+  */
+
+export default class ProgramVisitor extends ASTVisitor {
+  /** Visit Program node
+    * @param {Object} path
+    * @param {Object} state
+    */
+  enter(path, state) {
+
+  };
+}
