77742eb205b9ae083bac00cdb985462d2dad23bd
Ishi Ruy <dev@nhz.io>
2016-02-18 15:59:37 +0200

    #4 added new tests

--- test/unit/visitors/ExportNamedDeclarationVisitor.js
+++ test/unit/visitors/ExportNamedDeclarationVisitor.js
@@ -1,8 +1,12 @@
-import {Should, expect, assert} from "chai"
+import sinon from "sinon"
+import sinonChai from "sinon-chai"
+import chai, {Should, expect, assert} from "chai"
+
 import ASTVisitor from "../../../src/visitors/ASTVisitor"
 import ExportNamedDeclarationVisitor from "../../../src/visitors/ExportNamedDeclarationVisitor"
 
 Should()
+chai.use(sinonChai)
 
 /** @test {ExportNamedDeclarationVisitor} */
 describe("ExportNamedDeclarationVisitor", () => {
@@ -39,8 +43,98 @@ describe("ExportNamedDeclarationVisitor", () => {
     it('traverses nested visitors')
   })
 
-  describe("#exit(path, state)", () => {
+  describe("#enter(path, state)", () => {
+    let visitor, nestedVisitors, pathMock, stateMock
+    beforeEach(() => {
+      nestedVisitors = { Test: {} }
+      visitor = new ExportNamedDeclarationVisitor(nestedVisitors)
+      pathMock = {
+        traverse: sinon.spy(),
+        parent: {
+          type: "Program",
+        },
+      }
+      stateMock = {
+        mocktail: {
+          imports: [],
+          exports: [],
+          path: "test.js",
+          name: "test",
+          module: "test",
+        }
+      }
+    })
 
+    it('throws TypeError when no `path.parent`', () => {
+      pathMock.parent = null
+      expect(() => visitor.enter(pathMock, {})).to.throw(TypeError)
+    })
+
+    it('throws TypeError if `path.parent` is not `Program`', () => {
+      pathMock.parent = {}
+      expect(() => visitor.enter(pathMock, {})).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when no `path` is passed', () => {
+      expect(() => visitor.enter()).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` if `path` is invalid', () => {
+      expect(() => visitor.enter(null)).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when no state is passed', () => {
+      expect(() => visitor.enter(pathMock)).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when `state` is invalid', () => {
+      expect(() => visitor.enter(pathMock, null)).to.throw(TypeError)
+      expect(() => visitor.enter(pathMock, {})).to.throw(TypeError)
+      expect(() => visitor.enter(pathMock, {mocktail:null})).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when `state.mocktail.path` is invalid', () => {
+      stateMock.mocktail.path = null
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.path = ""
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.path = {}
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when `state.mocktail.name` is invalid', () => {
+      stateMock.mocktail.name = null
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.name = ""
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.name = {}
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+    })
+
+    it('throws `TypeError` when `state.mocktail.module` is invalid', () => {
+      stateMock.mocktail.module = null
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.module = ""
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+      stateMock.mocktail.module = {}
+      expect(() => visitor.enter(pathMock, stateMock)).to.throw(TypeError)
+    })
+
+    it('traverses nested visitors', () => {
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.have.been.calledOnce
+    })
+
+    it('does not traverse when there are no nested visitors', () => {
+      visitor = new ExportNamedDeclarationVisitor()
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.not.have.been.called
+    })
+
+    it('passes the state to nested visitors while traversing', () => {
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.have.been.calledWith(nestedVisitors, stateMock)
+    })
   })
 
 })
