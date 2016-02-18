e5b9b2f299ef708eab765365ca583195933f57f5
Ishi Ruy <dev@nhz.io>
2016-02-18 14:11:20 +0200

    #3 added new tests

--- test/unit/visitors/ProgramVisitor.js
+++ test/unit/visitors/ProgramVisitor.js
@@ -1,8 +1,12 @@
-import {Should, expect, assert} from "chai"
+import sinon from "sinon"
+import sinonChai from "sinon-chai"
+import chai, {Should, expect, assert} from "chai"
+
 import ASTVisitor from "../../../src/visitors/ASTVisitor"
 import ProgramVisitor from "../../../src/visitors/ProgramVisitor"
 
 Should()
+chai.use(sinonChai)
 
 /** @test {ProgramVisitor} */
 describe("ProgramVisitor", () => {
@@ -28,52 +32,95 @@ describe("ProgramVisitor", () => {
       visitor.nestedVisitors.One.should.be.equal(One)
       visitor.nestedVisitors.Two.should.be.equal(Two)
     })
-
-    it("creates the ~state property", () => {
-      visitor.state.should.be.ok
-      visitor.state.should.be.an.instanceof(Object)
-    })
-  })
-
-  describe("~state", () => {
-    let visitor
-    const properties = {
-      "name"    : String,
-      "absPath" : String,
-      "relPath" : String,
-      "imports" : Array,
-      "exports" : Array,
-    }
-    beforeEach(() => visitor = new ProgramVisitor())
-
-    Object.keys(properties).forEach((name) => {
-      const type = properties[name]
-      it(`has '${name}' x of type '${type.name}'`, () => {
-        let value = visitor.state[name]
-        assert((value instanceof type) || (typeof(value) === type.name.toLowerCase()))
-      })
-    })
-
   })
 
   /** @test {ProgramVisitor#enter} */
   describe("#enter(path, state)", () => {
-    let visitor
-    beforeEach(() => visitor = new ProgramVisitor())
+    let visitor, nestedVisitors, pathMock, stateMock
+    beforeEach(() => {
+      nestedVisitors = { Test: {} }
+      visitor = new ProgramVisitor(nestedVisitors)
+      pathMock = {
+        traverse: sinon.spy(),
+        parent: {
+          type: "File",
+          hub: { opts: { filename: "test.js" } }
+          }
+        }
+      stateMock = {}
+    })
 
-    it('sets the ~state.name property')
+    it('throws `TypeError` if no `path` is passed', () => {
+      expect(() => visitor.enter()).to.throw(TypeError)
+    })
 
-    it('sets the ~state.absPath property')
+    it('throws `TypeError` if `path` is invalid', () => {
+      expect(() => visitor.enter(null)).to.throw(TypeError)
+    })
 
-    it('sets the ~state.relPath property')
+    it('throws `TypeError` if no state is passed', () => {
+      expect(() => visitor.enter(pathMock)).to.throw(TypeError)
+    })
 
-    it('initializes the ~state.import property')
+    it('throws `TypeError` if `state` is invalid', () => {
+      expect(() => visitor.enter(pathMock, null)).to.throw(TypeError)
+    })
 
-    it('initializes the ~state.exports property')
+    it('throws Error if no `path.parent`', () => {
+      pathMock.parent = null
+      expect(() => visitor.enter(pathMock, {})).to.throw(Error)
+    })
 
-    it('traverses nested visitors')
+    it('throws Error if `path.parent` is not `File`', () => {
+      pathMock.parent = {}
+      expect(() => visitor.enter(pathMock, {})).to.throw(Error)
+    })
 
-    it('passes the state to nested visitors while traversing')
+    it('initializes the `state.mocktail`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail.should.be.ok.and.be.an('object')
+    })
+
+    it('initializes the `state.mocktail.imports`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail["imports"].should.be.ok.and.be.an.instanceOf(Array)
+    })
+
+    it('initializes the `state.mocktail.exports`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail["exports"].should.be.ok.and.be.an.instanceOf(Array)
+    })
+
+    it('sets the `state.mocktail.path`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail.path.should.be.equal("test.js")
+    })
+
+    it('sets the `state.mocktail.name`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail.name.should.be.equal("test")
+    })
+
+    it('sets the `state.mocktail.module`', () => {
+      visitor.enter(pathMock, stateMock)
+      stateMock.mocktail.module.should.be.equal("test")
+    })
+
+    it('traverses nested visitors', () => {
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.have.been.calledOnce
+    })
+
+    it('does not traverse if there are no nested visitors', () => {
+      visitor = new ProgramVisitor()
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.not.have.been.called
+    })
+
+    it('passes the state to nested visitors while traversing', () => {
+      visitor.enter(pathMock, stateMock)
+      pathMock.traverse.should.have.been.calledWith(nestedVisitors, stateMock)
+    })
   })
 
   describe("#exit(path, state)", () => {
