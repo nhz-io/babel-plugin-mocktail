76fc6617db297f6470ad48688230e25327c25283
Ishi Ruy <dev@nhz.io>
2016-02-18 19:45:37 +0200

    #16 turned ASTVisitor into test suite

--- test/unit/visitors/ASTVisitor.js
+++ test/unit/visitors/ASTVisitor.js
@@ -3,38 +3,64 @@ import ASTVisitor from "../../../src/visitors/ASTVisitor"
 
 Should()
 
-/** @test {ASTVisitor} */
-describe("ASTVisitor", () => {
+export default class ASTVisitorTests {
+  constructor({NAME, CLASS} = {}) {
+    this.NAME  = NAME || "ASTVisitor"
+    this.CLASS = CLASS || ASTVisitor
+  }
 
-  /** @test {ASTVisitor#constructor} */
-  describe("#constructor(nestedVisitors)", () => {
-    let visitor;
-    beforeEach(() => visitor = new ASTVisitor)
+  testConstructor() {
+    describe("#constructor(nestedVisitors)", () => {
+      const { NAME, CLASS } = this
+      let visitor;
+      beforeEach(() => visitor = new CLASS)
 
-    it("creates an instance of ASTVisitor", () => visitor.should.be.an.instanceOf(ASTVisitor))
+      it("creates an instance of ASTVisitor", () => visitor.should.be.an.instanceOf(ASTVisitor))
 
-    it("sets ~nestedVisitors", () => {
-      const One = {}, Two = {}, nestedVisitors = { One, Two }
+      it("sets ~nestedVisitors", () => {
+        const One = {}, Two = {}, nestedVisitors = { One, Two }
 
-      visitor = new ASTVisitor(nestedVisitors)
-      visitor.nestedVisitors.should.be.ok
-      visitor.nestedVisitors.should.not.be.equal(nestedVisitors)
-      visitor.nestedVisitors.One.should.be.equal(One)
-      visitor.nestedVisitors.Two.should.be.equal(Two)
+        visitor = new CLASS(nestedVisitors)
+        visitor.nestedVisitors.should.be.ok
+        visitor.nestedVisitors.should.not.be.equal(nestedVisitors)
+        visitor.nestedVisitors.One.should.be.equal(One)
+        visitor.nestedVisitors.Two.should.be.equal(Two)
+      })
     })
+  }
+
+  testEnter() {
+    describe("#enter", () => {
+      const { CLASS } = this
+      it("throws a SyntaxError", () => {
+        expect(() => (new CLASS).enter()).to.throw(SyntaxError)
+      })
+    })
+  }
+
+  testExit() {
+    describe("#exit", () => {
+      const { CLASS } = this
+      it("throws a SyntaxError", () => {
+        expect(() => (new CLASS).exit()).to.throw(SyntaxError)
+      })
+    })
+  }
+}
+
+if(!module.parent) {
+
+  /** @test {ASTVisitor} */
+  describe("ASTVisitor", () => {
+    const suite = new ASTVisitorTests()
+    /** @test {ASTVisitor#constructor} */
+    suite.testConstructor()
+
+    /** @TEST {ASTVisitor#enter} */
+    suite.testEnter()
+
+    /** @TEST {ASTVisitor#exit} */
+    suite.testExit()
   })
 
-  /** @test {ASTVisitor#enter} */
-  describe("#enter", () => {
-    it("throws a SyntaxError", () => {
-      expect(() => (new ASTVisitor).enter()).to.throw(SyntaxError)
-    })
-  })
-
-  /** @test {ASTVisitor#exit} */
-  describe("#exit", () => {
-    it("throws a SyntaxError", () => {
-      expect(() => (new ASTVisitor).exit()).to.throw(SyntaxError)
-    })
-  })
-})
+}
