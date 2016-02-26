15eefa607c13e688aab9d57c343eb0e26a764528
Ishi Ruy <dev@nhz.io>
2016-02-26 19:28:50 +0200

    new test stub for IdentifierVisitor

--- test/unit/visitors/IdentifierVisitor.js
+++ test/unit/visitors/IdentifierVisitor.js
@@ -1,77 +1,87 @@
-import sinon from "sinon"
+import chai, { expect } from "chai"
 import sinonChai from "sinon-chai"
-import chai, {Should, expect, assert} from "chai"
-
-import ASTVisitor from "../../../src/visitors/ASTVisitor"
-import NestedVisitor from "../../../src/visitors/NestedVisitor"
-import IdentifierVisitor from "../../../src/visitors/IdentifierVisitor"
-
-import ASTVisitorTests from "./ASTVisitor"
-import NestedVisitorTests from "./NestedVisitor"
-
-Should()
+import sinon from "sinon"
 chai.use(sinonChai)
 
-const NAME = "IdentifierVisitor"
-const CLASS = IdentifierVisitor
+import { transform, traverse } from "babel-core"
 
-export default class IdentifierVisitorTests {
+import IdentifierVisitor from "../../../src/visitors/IdentifierVisitor"
+
+export default class IdentifierVisitorTestSuite {
   constructor({NAME, CLASS} = {}) {
     this.NAME = NAME || "IdentifierVisitor"
     this.CLASS = CLASS || IdentifierVisitor
   }
 
   testConstructor() {
-    const { NAME, CLASS } = this
-    describe("#constructor()", () => {
-      let visitor
-      beforeEach(() => visitor = new CLASS())
-
-      it("creates an instance of IdentifierVisitor", () => {
-        visitor.should.be.an.instanceOf(IdentifierVisitor)
-      })
-    })
-
-  }
-
-  testEnter({parent} = {}) {
-    const { CLASS } = this
-    describe("#enter(path, state)", () => {
-      let path, state, visitor
-      beforeEach(() => {
-        visitor = new CLASS()
-        path = new PathMock({parent})
-        state = new StateMock()
-      })
-      
-      it("should register the ClassDeclaration export", () => {
-        path.parent
+    describe("Identifier#constructor()", () => {
+      it("should create an instance of IdentifierVisitor", () => {
+        expect(new IdentifierVisitor()).to.be.an.instanceOf(IdentifierVisitor)
       })
     })
   }
 
+  testEnter() {
+    describe('IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" })', () => {
+      it("should register the export", () => {
+        const identifierVisitor = new IdentifierVisitor()
+        const spy = sinon.spy()
+        const ast = transform("export default Test").ast
+        traverse(ast, { Identifier: new IdentifierVisitor()}, { export: spy })
+        spy.should.have.been.called
+      })
+    })
+
+    describe('IdentifierVisitor#enter(path, { type: "ExportSpecifier" })', () => {
+      it("should register the export", () => {
+        const identifierVisitor = new IdentifierVisitor()
+        const spy = sinon.spy()
+        const ast = transform("export default Test").ast
+        traverse(ast, { Identifier: new IdentifierVisitor()}, { export: spy })
+        spy.should.have.been.called
+      })
+    })
+
+    describe('IdentifierVisitor#enter(path, { type: "FunctionExpression" })', () => {
+      it("should register the export", () => {
+        const identifierVisitor = new IdentifierVisitor()
+        const spy = sinon.spy()
+        const ast = transform("export default Test").ast
+        traverse(ast, { Identifier: new IdentifierVisitor()}, { export: spy })
+        spy.should.have.been.called
+      })
+    })
+
+    describe('IdentifierVisitor#enter(path, { type: "ClassDeclaration" })', () => {
+      it("should register the export", () => {
+        const identifierVisitor = new IdentifierVisitor()
+        const spy = sinon.spy()
+        const ast = transform("export default Test").ast
+        traverse(ast, { Identifier: new IdentifierVisitor()}, { export: spy })
+        spy.should.have.been.called
+      })
+    })
+
+    describe('IdentifierVisitor#enter(path, { type: "VariableDeclarator" })', () => {
+      it("should register the export", () => {
+        const identifierVisitor = new IdentifierVisitor()
+        const spy = sinon.spy()
+        const ast = transform("export default Test").ast
+        traverse(ast, { Identifier: new IdentifierVisitor()}, { export: spy })
+        spy.should.have.been.called
+      })
+    })
+  }
 }
 
 /** @test {IdentifierVisitor} */
-describe("IdentifierVisitor", () => {
-  const astVisitorSuite = new ASTVisitorTests({NAME, CLASS})
-  const nestedVisitorSuite = new NestedVisitorTests({NAME, CLASS})
-  const identifierVisitorSuite = new IdentifierVisitorTests({NAME, CLASS})
 
-  it('is a subclass of ASTVisitor', () => {
-    IdentifierVisitor.prototype.should.be.an.instanceof(ASTVisitor)
-  })
+/** @test {IdentifierVisitor#constructor} */
 
-  it('is a subclass of NestedVisitor', () => {
-    IdentifierVisitor.prototype.should.be.an.instanceof(NestedVisitor)
-  })
+const testSuite = new IdentifierVisitorTestSuite()
 
-  /** @test {IdentifierVisitor#constructor} */
-  astVisitorSuite.testConstructor()
-  nestedVisitorSuite.testConstructor()
-  identifierVisitorSuite.testConstructor()
+testSuite.testConstructor()
 
-  /** @test {IdentifierVisitor#enter(path, state)} */
-  nestedVisitorSuite.testEnter()
-  identifierVisitorSuite.testEnter()
-})
+/** @test {IdentifierVisitor#enter(path, state)} */
+
+testSuite.testEnter()
