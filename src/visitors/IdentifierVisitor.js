af84f01261b1e0937ff500b26f31e9ace626d502
Ishi Ruy <dev@nhz.io>
2016-02-26 23:05:57 +0200

    real IdentifierVisitor tests + pass

--- src/visitors/IdentifierVisitor.js
+++ src/visitors/IdentifierVisitor.js
@@ -1,43 +1,28 @@
 /**
   * @example
-  * const visitor = new IdentifierVisitor()
+  * const visitor = new IdentifierVisitor({export: function(name, node) { ... }})
   * @see https://github.com/babel/babel/blob/master/doc/ast/spec.md#identifier
   * @see https://github.com/babel/babel/tree/master/packages/babel-types#tidentifiername
   * @see https://github.com/babel/babel/blob/5b89849f432cfc034860f495b5001039123e7754/packages/babel-types/src/definitions/core.js#L304
   */
 export default class IdentifierVisitor {
 
-  constructor() {
-    return function enter(path, state) {
+  static PARENTS = [
+    "ExportDefaultDeclaration",
+    "ExportSpecifier",
+    "FunctionDeclaration",
+    "ClassDeclaration",
+    "VariableDeclarator",
+  ]
+
+  constructor(defaultState = {}) {
+    return function enter(path, state = defaultState) {
       if(path.type === "Identifier") {
-        const parent = path.parent.type
-        switch(parent) {
-          case "ExportDefaultDeclaration":
-            state.export()
-            break
-
-          case "ExportSpecifier":
-            state.export()
-            break
-
-          case "FunctionExpression":
-            state.export()
-            break
-
-          case "FunctionDeclaration":
-            state.export()
-            break
-
-          case "ClassDeclaration":
-            state.export()
-            break
-
-          case "VariableDeclarator":
-            state.export()
-            break
-
-          default:
-            throw new TypeError("Invalid parent: " + parent)
+        for(let parent of IdentifierVisitor.PARENTS) {
+          if(parent === path.parent.type) {
+            state.export(path.node.name, path.node)
+            return
+          }
         }
       }
     }
--- test/unit/visitors/IdentifierVisitorTest.js
+++ test/unit/visitors/IdentifierVisitorTest.js
@@ -9,68 +9,163 @@ import { transform, traverse } from "babel-core"
 import IdentifierVisitor from "../../../src/visitors/IdentifierVisitor"
 
 describe("IdentifierVisitor", () => {
-  /** @test IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" }) */
-  describe('IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" })', () => {
-    it("should call the export()", () => {
-      const enter = new IdentifierVisitor()
-      const spy = sinon.spy()
-      const path = transform("export default Test").ast._paths[0]
+  let spy, enter, path
+
+  /** @test IdentifierVisitor */
+  describe("IdentifierVisitor#constructor(defaultState)", () => {
+    it("should return a function", () => {
+      expect(new IdentifierVisitor()).to.be.an.instanceOf(Function)
+    })
+  })
+
+  /** @test IdentifierVisitor#enter(path, state) - parent: ExportDefaultDeclaration */
+  describe("IdentifierVisitor#enter(path, state) - parent: ExportDefaultDeclaration", () => {
+    /** @test using default state */
+    it("should export the identifier using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export default Test").ast._paths[0]
+      path.traverse({ enter })
+      spy.should.have.been.calledWith("Test", path.node.body[0].declaration)
+    })
+
+    /** @test using provided state */
+    it("should export the identifier using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor()
+      path = transform("export default Test").ast._paths[0]
       path.traverse({ enter }, { export: spy })
-      spy.should.have.been.called
+      spy.should.have.been.calledWith("Test", path.node.body[0].declaration)
     })
   })
 
-  /** @test IdentifierVisitor#enter(path, { type: "ExportSpecifier" }) */
-  describe('IdentifierVisitor#enter(path, { type: "ExportSpecifier" })', () => {
-    it("should call the export()", () => {
-      const enter = new IdentifierVisitor()
-      const spy = sinon.spy()
-      const path = transform("export { test }").ast._paths[0]
+  /** @test IdentifierVisitor#enter(path, state) - parent: ExportSpecifier */
+  describe("IdentifierVisitor#enter(path, state) - parent: ExportSpecifier", () => {
+    /** @test using default state */
+    it("should export the identifier using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export { test }").ast._paths[0]
+      path.traverse({ enter })
+      spy.should.have.been.calledWith(
+        path.node.body[0].specifiers[0].exported.name,
+        path.node.body[0].specifiers[0].local
+      )
+    })
+
+    /** @test using provided state */
+    it("should export the identifier using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export { test }").ast._paths[0]
       path.traverse({ enter }, { export: spy })
-      spy.should.have.been.called
+      spy.should.have.been.calledWith(
+        path.node.body[0].specifiers[0].exported.name,
+        path.node.body[0].specifiers[0].local
+      )
     })
   })
 
-  /** @test IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" }) */
-  describe('IdentifierVisitor#enter(path, { type: "FunctionDeclaration" })', () => {
-    it("should call the export()", () => {
-      const enter = new IdentifierVisitor()
-      const spy = sinon.spy()
-      const path = transform("export function test() {}").ast._paths[0]
+  /** @test IdentifierVisitor#enter(path, state) - parent: FunctionDeclaration */
+  describe("IdentifierVisitor#enter(path, state) - parent: FunctionDeclaration", () => {
+    /** @test using default state */
+    it("should export the identifier using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export function test() {}").ast._paths[0]
+      path.traverse({ enter })
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.id.name,
+        path.node.body[0].declaration.id
+      )
+    })
+
+    /** @test using provided state */
+    it("should export the identifier using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export function test() {}").ast._paths[0]
       path.traverse({ enter }, { export: spy })
-      spy.should.have.been.called
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.id.name,
+        path.node.body[0].declaration.id
+      )
     })
   })
 
-  /** @test IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" }) */
-  describe('IdentifierVisitor#enter(path, { type: "ClassDeclaration" })', () => {
-    it("should call the export()", () => {
-      const enter = new IdentifierVisitor()
-      const spy = sinon.spy()
-      const path = transform("export class Test {}").ast._paths[0]
+  /** @test IdentifierVisitor#enter(path, state) - parent: ClassDeclaration */
+  describe("IdentifierVisitor#enter(path, state) - parent: ClassDeclaration", () => {
+    /** @test using default state */
+    it("should export the identifier using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export class Test {}").ast._paths[0]
+      path.traverse({ enter })
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.id.name,
+        path.node.body[0].declaration.id
+      )
+    })
+
+    /** @test using provided state */
+    it("should export the identifier using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export class Test {}").ast._paths[0]
       path.traverse({ enter }, { export: spy })
-      spy.should.have.been.called
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.id.name,
+        path.node.body[0].declaration.id
+      )
     })
   })
 
 
-  /** @test IdentifierVisitor#enter(path, { type: "ExportDefaultDeclaration" }) */
-  describe('IdentifierVisitor#enter(path, { type: "VariableDeclarator" })', () => {
-    it("should call the export()", () => {
-      const enter = new IdentifierVisitor()
-      const spy = sinon.spy()
-      const path = transform("export const test = null").ast._paths[0]
+  /** @test IdentifierVisitor#enter(path, state) - parent: VariableDeclarator */
+  describe("IdentifierVisitor#enter(path, state) - parent: VariableDeclarator", () => {
+    /** @test using default state */
+    it("should export the identifier using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export const test = null").ast._paths[0]
+      path.traverse({ enter })
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.declarations[0].id.name,
+        path.node.body[0].declaration.declarations[0].id
+      )
+    })
+
+    /** @test using provided state */
+    it("should export the identifier using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      path = transform("export const test = null").ast._paths[0]
       path.traverse({ enter }, { export: spy })
-      spy.should.have.been.called
+      spy.should.have.been.calledWith(
+        path.node.body[0].declaration.declarations[0].id.name,
+        path.node.body[0].declaration.declarations[0].id
+      )
     })
   })
 
-  /** @test IdentifierVisitor#enter(pathl, { type: InvalidType }) */
-  describe("IdentifierVisitor#enter(path, { type: InvalidType })", () => {
-    it("should throw a TypeError", () => {
-      const enter = new IdentifierVisitor()
-      expect(() => enter({ type: "Identifier", parent: { type: "InvalidType" } })).to.throw(TypeError)
+  /** @test IdentifierVisitor#enter(path, state) - parent: InvalidType */
+  describe("IdentifierVisitor#enter(path, state) - parent: InvalidType", () => {
+    /** @test using default state */
+    it("should not export anything using default state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      enter({ type: "Identifier", parent: { type: "InvalidType" } })
+      spy.should.not.have.been.called
+    })
+
+    /** @test using provided state */
+    it("should not export anything using provided state", () => {
+      spy = sinon.spy()
+      enter = new IdentifierVisitor()
+      spy = sinon.spy()
+      enter = new IdentifierVisitor({ export: spy })
+      enter({ type: "Identifier", parent: { type: "InvalidType" } }, { export: spy })
+      spy.should.not.have.been.called
     })
   })
-
 })
